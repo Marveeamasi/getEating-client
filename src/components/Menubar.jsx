57afde277d57a8f9244cb4ed99e46c6761bc07af
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2023-05-30 14:25:36 +0100

    Update Menubar.jsx

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -10,7 +10,7 @@ const Menubar = () => {
 
   return (
     <>
-    <div className='fixed glassmd w-10 h-10 right-2  z-50 p-5 flex justify-center items-center desk:hidden '>
+    <div className='fixed glassmd  w-10 h-10 sm:m-[10px] right-2  z-50 p-5 flex justify-center items-center desk:hidden '>
     {isOn? (<i onClick={toggle} class="fa-solid fa-xmark text-2xl text-[#ff0000] hover:text-[#ffae00]"></i>) 
     : (<i onClick={toggle} class="fa-solid fa-bars text-2xl text-[#ff0000] hover:text-[#ffae00]"></i>)} 
      
